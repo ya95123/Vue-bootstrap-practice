63f8b15b81549d0815013d7e873705ba7e52da7b
ya95123 <ya95123@gmail.com>
2020-05-19 14:44:22 +0800

    串財哥api

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template lang="pug">
+  //- 原Bootstrap寫法
   //- .container
   //-   .row
   //-     .col-3.bg-success
   //-     .col-3.bg-danger
   //-     .col-3.bg-warning
   //-     .col-3.bg-primary
-  b-container(fluid)
+
+  //- Bootstrap Vue 寫法
+  b-container
     b-row(align-h="center")
-      b-col.bg-success(cols="12" md="6" lg="3")
-      b-col.bg-danger(cols="12" md="6" lg="3")
-    //- b-col.bg-warning(col="3")
-    //- b-col.bg-primary(col="3")
+      b-col(cols="12" md="6" lg="4" xl="3")
 
 </template>
 
 <script>
 
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      cards: []
+    }
+  },
+  mounted () {
+    this.axios.get('https://api.kento520.tw/zack/')
+      .then(result => {
+        this.cards = result.data.slice(0, 10)
+        console.log(this.cards)
+      })
+  }
 
 }
 </script>
-
-<style lang="stylus" scoped>
-  .row div
-    height 300px
-</style>
--- src/main.js
+++ src/main.js
@@ -7,9 +7,16 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 // address-cared -> faAddressCard
 import { faAddressCard, faArrowUp } from '@fortawesome/free-solid-svg-icons'
+// axios 引用
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
+// font-awsome
 library.add(faAddressCard, faArrowUp)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+// 使用 axios
+Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
 
